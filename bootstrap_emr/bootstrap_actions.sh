92637e924906547ad33788a3174e3d3806107222
Gary Stafford <gary.a.stafford@gmail.com>
2020-12-12 11:47:50 -0500

    Refactoring script and debugging

--- bootstrap_emr/bootstrap_actions.sh
+++ bootstrap_emr/bootstrap_actions.sh
@@ -19,25 +19,33 @@ sudo -u hadoop -i bash -c "python3 -m pip install --user --upgrade setuptools pi
 sudo -u hadoop -i bash -c "python3 -m venv venv"
 sudo -u hadoop -i bash -c ". venv/bin/activate"
 
+PYTHON_DIR="$(ls /home/hadoop/venv/lib | head)" # should only be one dir in there
+echo "PYTHON_DIR: ${PYTHON_DIR}"
+
+SUPERSET_HOME="/home/hadoop/venv/lib/${PYTHON_DIR}/site-packages/superset"
+echo "SUPERSET_HOME: ${SUPERSET_HOME}"
+
+sudo -u hadoop -i bash -c "echo \"export SUPERSET_HOME=${SUPERSET_HOME}\" >> ~/.bashrc"
+
 # install and upgrade superset
-sudo -u hadoop -i bash -c "python3 -m pip install apache-superset"
+sudo -u hadoop -i bash -c "python3 -m pip install --target=/home/hadoop/venv/bin apache-superset"
 sudo -u hadoop -i bash -c "superset db upgrade"
 
 # install a few db drivers: Amazon Athena, RedShift, Spark SQL/Presto, PostgreSQL, MySQL
 # https://superset.apache.org/docs/databases/installing-database-drivers
-sudo -u hadoop -i bash -c "python3 -m pip install PyAthenaJDBC>1.0.9 PyAthena>1.2.0 sqlalchemy-redshift pyhive mysqlclient"
+sudo -u hadoop -i bash -c "python3 -m pip install --target=/home/hadoop/venv/bin PyAthenaJDBC>1.0.9 PyAthena>1.2.0 sqlalchemy-redshift pyhive mysqlclient"
 
 sudo -u hadoop -i bash -c "echo \"export FLASK_APP=superset\" >> ~/.bashrc"
 
-PYTHON_DIR="$(ls ~/venv/lib/ | head 1)" # should only be one dir in there
-sudo -u hadoop -i bash -c "echo \"export SUPERSET_HOME=~/venv/lib/${PYTHON_DIR}/site-packages/superset/\" >> ~/.bashrc"
-
 # enabling logging
 # e.g. location: /home/hadoop/venv/lib64/python3.7/site-packages/superset/superset.log
-sudo -u hadoop -i bash -c "touch superset_config.py"
+SUPERSET_CONFIG_FILE="${SUPERSET_HOME}/superset_config.py"
+echo "SUPERSET_CONFIG_FILE: ${SUPERSET_CONFIG_FILE}"
+
+sudo -u hadoop -i bash -c "touch ${SUPERSET_CONFIG_FILE}"
 sudo -u hadoop -i bash -c """
-  echo \"ENABLE_TIME_ROTATE = True\" >> \"\~/venv/lib/${PYTHON_DIR}/site-packages/superset/superset_config.py\""""
-sudo -u hadoop -i bash -c "echo \"export SUPERSET_CONFIG_PATH=~/superset_config.py\" >> ~/.bashrc"
+  echo \"ENABLE_TIME_ROTATE = True\" >> \"${SUPERSET_CONFIG_FILE}\""""
+sudo -u hadoop -i bash -c "echo \"export SUPERSET_CONFIG_PATH=${SUPERSET_CONFIG_FILE}\" >> ~/.bashrc"
 
 ADMIN_USERNAME="SupersetAdmin"
 ADMIN_PASSWORD="Admin1234"
@@ -76,9 +84,11 @@ sudo -u hadoop -i bash -c """
 
 # get instance id
 INSTANCE_ID="$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .instanceId)"
+echo "INSTANCE_ID: ${INSTANCE_ID}"
 
 # use instance id to get public dns of master node
 PUBLIC_MASTER_DNS="$(aws ec2 describe-instances --instance-id ${INSTANCE_ID} | jq -r '.Reservations[0].Instances[0].PublicDnsName')"
+echo "PUBLIC_MASTER_DNS: ${PUBLIC_MASTER_DNS}"
 
 # start superset in background
 sudo -u hadoop -i bash -c """
---
+++ delete_s3_buckets.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Delete Super EMR Demo buckets
+# Author: Gary A. Stafford (November 2020)
+
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+
+s3_client = boto3.resource('s3')
+sts_client = boto3.client('sts')
+
+
+def main():
+    region = boto3.DEFAULT_SESSION.region_name
+    account_id = sts_client.get_caller_identity()['Account']
+
+    bootstrap_bucket = f'superset-emr-demo-bootstrap-{account_id}-{region}'
+    logs_bucket = f'superset-emr-demo-logs-{account_id}-{region}'
+
+    delete_buckets([bootstrap_bucket, logs_bucket])
+
+
+def delete_buckets(buckets):
+    """ Delete all Amazon S3 buckets created for this project """
+
+    for bucket in buckets:
+        try:
+            bucket_to_delete = s3_client.Bucket(bucket)
+            bucket_to_delete.object_versions.delete()
+            bucket_to_delete.delete()
+            print(f"Bucket deleted: {bucket}")
+        except ClientError as e:
+            logging.error(e)
+
+
+if __name__ == '__main__':
+    main()
