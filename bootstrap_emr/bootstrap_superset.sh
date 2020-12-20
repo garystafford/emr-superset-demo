cfd8843aae0957db558bc1427ad4d488e455fab7
Gary Stafford <gary.a.stafford@gmail.com>
2020-12-19 20:31:40 -0500

    Refactor boot strap script

--- bootstrap_emr/bootstrap_superset.sh
+++ bootstrap_emr/bootstrap_superset.sh
@@ -6,18 +6,17 @@
 # Reference: https://superset.apache.org/docs/installation/installing-superset-from-scratch
 
 # port for superset (default: 8280)
-export SUPERSET_PORT="${1:-8280}"
+SUPERSET_PORT="${1:-8280}"
 
 # install required packages
 sudo yum -y install gcc gcc-c++ libffi-devel python-devel python-pip python-wheel \
   openssl-devel cyrus-sasl-devel openldap-devel python3-devel.x86_64
 
-# optionally, update Master Node packages
+# optionally, update master node packages
 sudo yum -y update
 
-# install required Python package
+# set up a python virtual environment
 python3 -m pip install --user --upgrade setuptools virtualenv
-
 python3 -m venv venv
 . venv/bin/activate
 
@@ -28,15 +27,16 @@ command -v superset
 
 superset db upgrade
 
-export FLASK_APP=superset
+FLASK_APP=superset
+export "FLASK_APP=${FLASK_APP}"
 echo "export FLASK_APP=superset" >>~/.bashrc
 
 touch superset_config.py
 echo "ENABLE_TIME_ROTATE = True" >>superset_config.py
 echo "export SUPERSET_CONFIG_PATH=superset_config.py" >>~/.bashrc
 
-export ADMIN_USERNAME="SupersetAdmin"
-export ADMIN_PASSWORD="Admin1234"
+ADMIN_USERNAME="SupersetAdmin"
+ADMIN_PASSWORD="Admin1234"
 
 # create superset admin
 superset fab create-admin \
@@ -65,16 +65,12 @@ superset fab create-user \
   --email superset_user_gamma@example.com \
   --password UserGamma1234
 
-# get instance id
+# get ec2 instance id
 INSTANCE_ID="$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .instanceId)"
-export INSTANCE_ID
-echo "INSTANCE_ID: ${INSTANCE_ID}"
 
-# use instance id to get public dns of master node
+# use ec2 instance id to get public dns of master node
 PUBLIC_MASTER_DNS="$(aws ec2 describe-instances --instance-id ${INSTANCE_ID} |
   jq -r '.Reservations[0].Instances[0].PublicDnsName')"
-export PUBLIC_MASTER_DNS
-echo "PUBLIC_MASTER_DNS: ${PUBLIC_MASTER_DNS}"
 
 # start superset in background
 nohup superset run \
@@ -83,7 +79,7 @@ nohup superset run \
   --with-threads --reload --debugger \
   >superset_output.log 2>&1 </dev/null &
 
-# output connection info
+# output superset connection info
 printf %s """
 **********************************************************************
   Superset URL: http://${PUBLIC_MASTER_DNS}:${SUPERSET_PORT}
---
+++ install_airflow.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+# Purpose: Install Apache Superset on EMR Master Node
+# Author:  Gary A. Stafford (December 2020)
+# Usage Example: python3 ./create_cfn_stack.py \
+#                    --ec2-key-path ~/.ssh/emr-demo-123456789012-us-east-1.pem \
+#                    --superset-port 8280
+
+import argparse
+import logging
+import os
+
+import boto3
+from paramiko import SSHClient, AutoAddPolicy
+from scp import SCPClient
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)s - %(message)s', level=logging.INFO)
+
+ssm_client = boto3.client('ssm')
+
+
+def main():
+    args = parse_args()
+    params = get_parameters()
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file = f'{dir_path}/bootstrap_emr/bootstrap_superset.sh'
+
+    # upload bootstrap script
+    install_superset(file, params['master_public_dns'], 'hadoop', args.ec2_key_path, args.superset_port)
+
+
+def install_superset(file, master_node_dns, username, ec2_key_path, superset_port):
+    """SCP script to EMR Master Node and run"""
+
+    ssh = SSHClient()
+    ssh.load_system_host_keys()
+    ssh.set_missing_host_key_policy(AutoAddPolicy())
+
+    ssh.connect(hostname=master_node_dns, username=username, key_filename=ec2_key_path)
+
+    with SCPClient(ssh.get_transport()) as scp:
+        scp.put(file)
+
+    stdin_, stdout_, stderr_ = ssh.exec_command(
+        command=f'sh ./bootstrap_superset.sh {superset_port} 2>&1')
+
+    stdout_lines = ''
+    while not stdout_.channel.exit_status_ready():
+        if stdout_.channel.recv_ready():
+            stdout_lines = stdout_.readlines()
+    logging.info(' '.join(map(str, stdout_lines)))
+
+    ssh.close()
+
+
+def get_parameters():
+    """Load parameter values from AWS Systems Manager (SSM) Parameter Store"""
+
+    params = {
+        'master_public_dns': ssm_client.get_parameter(Name='/emr_superset_demo/master_public_dns')['Parameter']['Value']
+    }
+
+    return params
+
+
+def parse_args():
+    """Parse argument values from command-line"""
+
+    parser = argparse.ArgumentParser(description='Arguments required for script.')
+    parser.add_argument('-e', '--ec2-key-path', required=True, help='EC2 Key Path')
+    parser.add_argument('-s', '--superset-port', default='8280', help='Apache Superset Port')
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    main()
